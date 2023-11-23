63762bcd3c29fb5a8fd6628127fdb95fc4cd7853
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-11-22 22:47:13 -0400

    transactions lazycolumn com cards

--- app/src/main/java/com/example/finance/MainActivity.kt
+++ app/src/main/java/com/example/finance/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.finance.ui.theme.FinanceTheme
@@ -62,13 +68,22 @@ fun Welcome(){
 
 @Composable
 fun Transactions(){
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ){
-        transactionsDummy.forEach{ transaction ->
-            Text(
-                text = transaction
-            )
+    LazyColumn(
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(transactionsDummy.size) { index ->
+            Card(
+                elevation = CardDefaults.cardElevation(8.dp),
+                modifier = Modifier.fillMaxWidth()
+            ){
+                Text(
+                    text = transactionsDummy[index],
+                    modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
