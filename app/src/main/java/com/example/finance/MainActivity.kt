8fada1bf48ae6728aadf31eacb2e2777fae93ae1
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-11-23 23:33:49 -0400

    ViewModel contendo StateFlow com as Transactions

--- app/src/main/java/com/example/finance/MainActivity.kt
+++ app/src/main/java/com/example/finance/MainActivity.kt
@@ -6,25 +6,45 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finance.ui.theme.FinanceTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MyViewModel : ViewModel(){
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _uiState
+
+    fun add(transaction: String){
+        val transactions = _uiState.value.transactions.toMutableList()
+        transactions.add(transaction)
+        _uiState.value = UiState(transactions = transactions)
+    }
+
+    data class UiState(
+        val transactions: List<String> = emptyList()
+    )
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,19 +89,30 @@ fun Welcome(){
 }
 
 @Composable
-fun Transactions(){
-    LazyColumn(
-        modifier = Modifier.padding(16.dp).fillMaxWidth(),
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        items(transactionsDummy.size) { index ->
-            Transaction(index)
+fun Transactions(viewModel: MyViewModel = viewModel()){
+    val uiState by viewModel.uiState.collectAsState()
+    Column{
+        LazyColumn(
+            modifier = Modifier
+                .padding(16.dp)
+                .weight(1f)
+                .fillMaxWidth(),
+                contentPadding = PaddingValues(8.dp)
+        ) {
+            items(uiState.transactions.size) { index ->
+                Transaction(uiState.transactions[index])
+            }
+        }
+        Button(onClick = {
+            viewModel.add("Nova Transação")
+        }){
+            Text(text = "Adicionar nova Transação")
         }
     }
 }
 
 @Composable
-private fun Transaction(index: Int) {
+private fun Transaction(transaction: String) {
     Card(
         elevation = CardDefaults.cardElevation(8.dp),
         modifier = Modifier
@@ -89,10 +120,8 @@ private fun Transaction(index: Int) {
             .padding(16.dp)
     ) {
         Row {
-            Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
-            Spacer(modifier = Modifier.padding(16.dp))
             Text(
-                text = transactionsDummy[index],
+                text = transaction,
                 modifier = Modifier
                     .padding(8.dp)
                     .fillMaxWidth(),
@@ -102,7 +131,3 @@ private fun Transaction(index: Int) {
         }
     }
 }
-
-private val transactionsDummy = listOf<String>(
-    "Cursos", "Passagens", "Mercado", "Café", "Academia"
-)
