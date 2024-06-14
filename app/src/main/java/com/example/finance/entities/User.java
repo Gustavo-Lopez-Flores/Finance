8675150504e4945b419130d2642ec0d111f87e71
Gustavo Lopez Flores <gustavo.flores@dominio.ufms.br>
2024-06-14 12:35:21 -0400

    criação de pacote entity e classe User

---
+++ app/src/main/java/com/example/finance/entities/User.java
@@ -0,0 +1,59 @@
+package com.example.finance.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int userId;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public User() {}
+
+    public User(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return userId + ": " + nome + ", " + email;
+    }
+}
