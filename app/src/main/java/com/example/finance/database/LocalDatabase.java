be286d0c9e26bea5167a7ecdecbe2f4daaf090d1
Gustavo Lopez Flores <gustavo.flores@dominio.ufms.br>
2024-06-19 12:36:23 -0400

    criação de LocalDatabase

--- app/src/main/java/com/example/finance/database/LocalDatabase.java
+++ app/src/main/java/com/example/finance/database/LocalDatabase.java
@@ -1,2 +1,19 @@
-package com.example.finance.database;public class LocalDatabase {
+package com.example.finance.database;
+
+import static java.time.chrono.ThaiBuddhistChronology.INSTANCE;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+public abstract class LocalDatabase extends RoomDatabase{
+    private static LocalDatabase INSTANCE;
+    public static LocalDatabase getDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    LocalDatabase.class, "MeuBD").allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
 }
