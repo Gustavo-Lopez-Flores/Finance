393279a775696ae8da5ad6655ef399fad7bc3e8c
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-26 00:06:16 -0400

    criação de entity Categoria

---
+++ app/src/main/java/com/example/finance/entities/Categoria.java
@@ -0,0 +1,45 @@
+package com.example.finance.entities;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(foreignKeys = @ForeignKey(entity = User.class, parentColumns = "id", childColumns = "usuarioId", onDelete = ForeignKey.CASCADE))
+public class Categoria {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int usuarioId;
+    private String nomeCategoria;
+
+    public Categoria() {}
+
+    public Categoria(int usuarioId, String nomeCategoria) {
+        this.usuarioId = usuarioId;
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+}
