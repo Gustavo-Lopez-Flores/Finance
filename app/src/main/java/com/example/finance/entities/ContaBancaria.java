0eafaa9792681eab56ca17a57b73980a9831eda7
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-25 23:46:47 -0400

    criação de entity ContaBancaria

---
+++ app/src/main/java/com/example/finance/entities/ContaBancaria.java
@@ -0,0 +1,55 @@
+package com.example.finance.entities;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(foreignKeys = @ForeignKey(entity = Usuario.class, parentColumns = "id", childColumns = "usuarioId", onDelete = ForeignKey.CASCADE))
+public class ContaBancaria {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int usuarioId;
+    private String nomeBanco;
+    private double saldo;
+
+    public ContaBancaria() {}
+
+    public ContaBancaria(int usuarioId, String nomeBanco, double saldo) {
+        this.usuarioId = usuarioId;
+        this.nomeBanco = nomeBanco;
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getNomeBanco() {
+        return nomeBanco;
+    }
+
+    public void setNomeBanco(String nomeBanco) {
+        this.nomeBanco = nomeBanco;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
