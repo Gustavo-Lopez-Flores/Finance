5bd6a74b9568f41df4c84b2020fe49fef18d4841
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-25 23:12:58 -0400

    instância de LocalDatabase, uso de Binding no onCreate e uso da lib Uri para a foto

--- app/src/main/java/com/example/finance/User/UserActivity.java
+++ app/src/main/java/com/example/finance/User/UserActivity.java
@@ -2,15 +2,28 @@ package com.example.finance.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.example.finance.R;
+import com.example.finance.database.LocalDatabase;
+import com.example.finance.databinding.ActivityUserBinding;
 
 public class UserActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_PICK = 1;
+    private static final int REQUEST_PERMISSION = 2;
+
+    private LocalDatabase db;
+    private ActivityUserBinding binding;
+    private Uri fotoPerfilUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        binding = ActivityUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        db = LocalDatabase.getDatabase(getApplicationContext());
+
     }
 }
