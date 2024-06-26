9345c3eae426ec41c4356e12ca067f5eca6ccba0
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-25 22:43:25 -0400

    uso de User em LocalDatabase

--- app/src/main/java/com/example/finance/database/LocalDatabase.java
+++ app/src/main/java/com/example/finance/database/LocalDatabase.java
@@ -1,11 +1,16 @@
 package com.example.finance.database;
 
+import com.example.finance.entities.User;
+
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = {User.class}, version = 1)
 public abstract class LocalDatabase extends RoomDatabase{
+    public abstract User usuarioDao();
     private static LocalDatabase INSTANCE;
     public static LocalDatabase getDatabase(Context context) {
         if(INSTANCE == null) {
