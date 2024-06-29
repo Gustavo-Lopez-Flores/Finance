bfce78352ffc004b171e6abe3292709934baeb9a
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-29 18:09:26 -0400

    insertConta com DAO insert

--- app/src/main/java/com/example/finance/view/contas/ContasViewModel.java
+++ app/src/main/java/com/example/finance/view/contas/ContasViewModel.java
@@ -3,15 +3,11 @@ package com.example.finance.view.contas;
 import android.app.Application;
 
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.example.finance.database.LocalDatabase;
 import com.example.finance.entities.ContaBancaria;
 
 public class ContasViewModel extends AndroidViewModel {
-
-    private final MutableLiveData<ContaBancaria> conta = new MutableLiveData<>();
     private final LocalDatabase db;
 
     public ContasViewModel(Application application) {
@@ -19,10 +15,7 @@ public class ContasViewModel extends AndroidViewModel {
         db = LocalDatabase.getDatabase(application);
     }
 
-    private void carregarContas() {
-    }
-
-    public LiveData<ContaBancaria> getContas() {
-        return conta;
+    public void insertConta(ContaBancaria conta) {
+        new Thread(() -> db.contaBancariaDao().insert(conta)).start();
     }
 }
