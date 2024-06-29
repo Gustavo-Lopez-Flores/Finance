af408e30703fcff02ff70b128b97db73863aba6b
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-29 16:10:20 -0400

    finish em Voltar

--- app/src/main/java/com/example/finance/view/user/UserUpdateActivity.java
+++ app/src/main/java/com/example/finance/view/user/UserUpdateActivity.java
@@ -31,7 +31,7 @@ public class UserUpdateActivity extends AppCompatActivity {
         binding.btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                finish();
             }
         });
 
