27480eca8415fc81d546294ca7e95c6b489837d2
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-29 15:47:28 -0400

    remoção de R para main

--- app/src/main/java/com/example/finance/view/user/UserUpdateActivity.java
+++ app/src/main/java/com/example/finance/view/user/UserUpdateActivity.java
@@ -17,10 +17,5 @@ public class UserUpdateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_update);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
