3e1429c89924babde435c58a9a44730336532161
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-29 16:09:50 -0400

    adição de bindindo e setOnClickListener de btn Confirmar, Carregar foto e Voltar

--- app/src/main/java/com/example/finance/view/user/UserUpdateActivity.java
+++ app/src/main/java/com/example/finance/view/user/UserUpdateActivity.java
@@ -1,21 +1,45 @@
 package com.example.finance.view.user;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.finance.R;
+import com.example.finance.databinding.ActivityUserUpdateBinding;
 
 public class UserUpdateActivity extends AppCompatActivity {
+    private ActivityUserUpdateBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_update);
+        binding = ActivityUserUpdateBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int userId = getIntent().getIntExtra("userId", -1);
+        if (userId != -1) {
+
+        }
+
+        binding.btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.btnSelecionarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
