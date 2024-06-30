481d9cb76b705abe1eaa325ecc176e8d9440189c
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-06-29 20:06:08 -0400

    criação de activitys

---
+++ app/src/main/java/com/example/finance/view/contas/ContasActivity.java
@@ -0,0 +1,21 @@
+package com.example.finance.view.contas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.finance.R;
+
+public class ContasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contas);
+    }
+}
